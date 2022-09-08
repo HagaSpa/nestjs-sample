d76dd06da465f143f51792d8494b9eaa0fcac343
HagaSpa <justicesparrow13@gmail.com>
2022-09-08 21:03:06 +0900

    change : addd Get cat by ID

--- nestjs-sample/src/cats/cats.controller.ts
+++ nestjs-sample/src/cats/cats.controller.ts
@@ -1,16 +1,21 @@
-import { Controller, Get, HttpCode, Post, Query,} from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, Post, Query,} from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
   @Post()
   @HttpCode(204)
   create(): string {
-    return 'This action adds a new cat'
+    return 'This action adds a new cat';
   }
   
   @Get()
   findAll(@Query('name') name: string): string {
     console.log('name :', name)
-    return 'This action returns all cats'
+    return 'This action returns all cats';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} cat`;
   }
 }
