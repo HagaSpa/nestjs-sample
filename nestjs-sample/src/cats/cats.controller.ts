1210c461b4e3ab35aa0f0cae3568f0be49a93c16
HagaSpa <justicesparrow13@gmail.com>
2022-10-04 19:51:25 +0900

    feat : add inject service

--- nestjs-sample/src/cats/cats.controller.ts
+++ nestjs-sample/src/cats/cats.controller.ts
@@ -1,25 +1,19 @@
 import { Body, Controller, Get, HttpCode, Param, Post, Query,} from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { Cat} from './interfaces/cat.interface'
 import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
+
   @Post()
-  @HttpCode(204)
   async create(@Body() createCatDto: CreateCatDto) {
-    console.log(createCatDto.age);
-    console.log(createCatDto.name);
-    console.log(createCatDto.breed);
-    return 'This action adds a new cat';
-  }
-  
-  @Get()
-  findAll(@Query('name') name: string): string {
-    console.log('name :', name)
-    return 'This action returns all cats';
+    this.catsService.create(createCatDto);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string): string {
-    return `This action returns a #${id} cat`;
+  @Get()
+  async findAll(): Promise<Cat[]> { 
+    return this.catsService.findAll();
   }
 }
